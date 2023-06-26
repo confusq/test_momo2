e98c1c8c5fb3221633920ca2b3c694ad4af69f7a
confusq <78801788+confusq@users.noreply.github.com>
2023-06-26 13:24:39 +0000

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -19,11 +19,11 @@ const MMSLS = [
 =====================请在下面编辑=====================
  */
 const MMSLS = [
-    "https://www.maimemo.com/share/page?uid=7843145&pid=7ba87f9c8ade1a34ab12848e75aaca38&tid=60e6ac024af08ee7f9269aa0236534dc",   
-    "https://www.maimemo.com/share/page?uid=7843145&pid=7ba87f9c8ade1a34ab12848e75aaca38&tid=60e6ac024af08ee7f9269aa0236534dc",
-    "https://www.maimemo.com/share/page?uid=7843145&pid=7ba87f9c8ade1a34ab12848e75aaca38&tid=60e6ac024af08ee7f9269aa0236534dc",
-    /* 我的*/
-    "https://www.maimemo.com/share/page?uid=5390766&pid=7450e7a7bbda02a444ce4ff88b9d3305&tid=db99acf3d648dd0ff5ec820f1b7dfce2",  
+    "https://www.maimemo.com/share/page?uid=7843145&pid=7450e7a7bbda02a444ce4ff88b9d3305&tid=318577015ef3364ecae7bdfd80b02f4c",
+    "https://www.maimemo.com/share/page?uid=7843145&pid=7450e7a7bbda02a444ce4ff88b9d3305&tid=318577015ef3364ecae7bdfd80b02f4c",
+    "https://www.maimemo.com/share/page?uid=7843145&pid=7450e7a7bbda02a444ce4ff88b9d3305&tid=318577015ef3364ecae7bdfd80b02f4c",
+    
+
   ]
   /**
    * 生成随机数字
