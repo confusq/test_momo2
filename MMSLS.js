655099c6969298eb23e8d93e4ab4441b04a031f7
confusq <78801788+confusq@users.noreply.github.com>
2023-06-26 13:37:35 +0000

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -22,6 +22,8 @@ const MMSLS = [
     "https://www.maimemo.com/share/page?uid=7843145&pid=7450e7a7bbda02a444ce4ff88b9d3305&tid=318577015ef3364ecae7bdfd80b02f4c",
     "https://www.maimemo.com/share/page?uid=7843145&pid=7450e7a7bbda02a444ce4ff88b9d3305&tid=318577015ef3364ecae7bdfd80b02f4c",
     "https://www.maimemo.com/share/page?uid=7843145&pid=7450e7a7bbda02a444ce4ff88b9d3305&tid=318577015ef3364ecae7bdfd80b02f4c",
+    "https://www.maimemo.com/share/page?uid=5390766&pid=7450e7a7bbda02a444ce4ff88b9d3305&tid=db99acf3d648dd0ff5ec820f1b7dfce2",
+    
     
 
   ]
