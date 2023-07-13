4ebd35000430e2151943791ffd73494f91cce1fb
confusq <78801788+confusq@users.noreply.github.com>
2023-07-13 09:50:58 +0800

    Update MMSLS.js

--- MMSLS.js
+++ MMSLS.js
@@ -19,7 +19,7 @@ const MMSLS = [
 =====================请在下面编辑=====================
  */
 const MMSLS = [
-    "https://www.maimemo.com/share/page?uid=5390766&pid=757548a3bc2f216fd39a581fa056654c&tid=42f8afea463cd9407aec4f6005103cf9",
+    "https://www.maimemo.com/share/page?uid=5390766&pid=5057877bdd190e100a06b28093653797&tid=a318575e7b7ea21b92eb851bb72dc4f4",
     "https://www.maimemo.com/share/page?uid=7843145&pid=757548a3bc2f216fd39a581fa056654c&tid=2e7b151fa79b64adb997d73a494061e2",
    
     
